83ffd4a7d280f88f8a541cbfe101847e2267f984
Armando Pacheco <aeiglesiass@gmail.com>
2019-01-31 19:13:32 -0500

    Some change into vue components

--- PhoneClient/src/views/About.vue
+++ PhoneClient/src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <HelloWorld msg="About me"></HelloWorld>
   </div>
 </template>
+
+<script>
+import HelloWorld from "../components/HelloWorld";
+
+export default {
+  name: "about",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
